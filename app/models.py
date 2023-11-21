7aad9084c901fa8154ed29d0bdb6dcc8e772dd93
Vladimir_Djobirov <djobirov05@mail.ru>
2023-11-21 20:20:31 +0500

    Reaction in posts, sort posts, Users leaderboard

--- app/models.py
+++ app/models.py
@@ -3,13 +3,13 @@ import re
 
 
 class User:
-    def __init__(self, id, first_name, last_name, email, total_reactions=0, posts=[]):
+    def __init__(self, id, first_name, last_name, email, total_reactions=0):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.total_reactions = total_reactions
-        self.posts = posts
+        self.total_reactions = 0
+        self.posts = []
 
     @staticmethod
     def is_valid_email(email):
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from app import app, USERS, models
 from flask import request, Response
-import json
 from http import HTTPStatus
+import json
+import copy
 
 
 @app.route("/")
@@ -21,6 +22,7 @@ def user_create():
         return Response(status=HTTPStatus.BAD_REQUEST)
 
     user = models.User(id, first_name, last_name, email)
+
     USERS.append(user)
     response = Response(
         json.dumps(
@@ -66,18 +68,25 @@ def get_user(user_id):
 def post_create():
     data = request.get_json()
     author_id = data["author_id"]
+
     if author_id < 0 or author_id >= len(USERS):
         return Response(status=HTTPStatus.NOT_FOUND)
-    post_id = len(USERS[int(author_id)].posts)
+
+    user = USERS[int(author_id)]
     text = data["text"]
+
+    post_id = len(user.posts)
+
     post = models.Post(author_id, text)
     post_to_dict = {
         "post_id": post_id,
         "author_id": post.author_id,
         "text": post.text,
-        "reactions": post.reactions,
+        "reactions": [],
     }
-    USERS[int(author_id)].posts.append(post_to_dict)
+
+    user.posts.append(post_to_dict)
+
     response = Response(
         json.dumps(
             {
@@ -91,4 +100,95 @@ def post_create():
         status=HTTPStatus.CREATED,
     )
 
+    return response
+
+
+@app.get("/post/<int:user_id>/<int:post_id>")
+def get_post(user_id, post_id):
+    if (user_id < 0 or user_id >= len(USERS)) or (
+        post_id < 0 or post_id > len(USERS[user_id].posts)
+    ):
+        return Response(status=HTTPStatus.NOT_FOUND)
+    post = USERS[user_id].posts[post_id]
+    response = Response(
+        json.dumps(
+            {
+                "post_id": post["post_id"],
+                "author_id": post["author_id"],
+                "text": post["text"],
+                "reactions": post["reactions"],
+            }
+        ),
+        status=HTTPStatus.CREATED,
+        mimetype="application/json",
+    )
+
+    return response
+
+
+@app.post("/post/<author_id>/<int:post_id>/reaction")
+def reaction(author_id, post_id):
+    data = request.get_json()
+    user_id = int(data["user_id"])
+    if user_id < 0 or user_id >= len(USERS):
+        return Response(status=HTTPStatus.NOT_FOUND)
+    reaction = data["reaction"]
+    if reaction not in ["like", "dislike"]:
+        return Response(status=HTTPStatus.BAD_REQUEST)
+    post = USERS[int(author_id)].posts[post_id]
+    reactions_to_dict = {
+        "user_id": user_id,
+        "reaction": reaction,
+    }
+    post["reactions"].append(reactions_to_dict)
+    USERS[user_id].total_reactions += 1
+    return Response(status=HTTPStatus.CREATED)
+
+
+@app.get("/user/<int:user_id>/posts")
+def get_posts(user_id):
+    data = request.get_json()
+    sort = data["sort"]
+    user = USERS[user_id]
+    posts = user.posts
+    if sort == "asc":
+        sorted_posts = sorted(posts, key=lambda x: len(x["reactions"]))
+    elif sort == "desc":
+        sorted_posts = sorted(posts, key=lambda x: len(x["reactions"]), reverse=True)
+    else:
+        return Response(status=HTTPStatus.BAD_REQUEST)
+    response = Response(
+        json.dumps({"posts": sorted_posts}),
+        status=HTTPStatus.CREATED,
+        mimetype="application/json",
+    )
+    return response
+
+
+@app.get("/users/leaderboard")
+def get_users():
+    data = request.get_json()
+    sort = data["sort"]
+    if sort == "asc":
+        sorted_users = sorted(USERS, key=lambda x: x.total_reactions)
+    elif sort == "desc":
+        sorted_users = sorted(USERS, key=lambda x: x.total_reactions, reverse=True)
+    else:
+        return Response(status=HTTPStatus.BAD_REQUEST)
+
+    users_copy = copy.deepcopy(sorted_users)
+    for user in users_copy:
+        user.__dict__.pop("posts", None)
+
+    response = Response(
+        json.dumps(
+            {
+                "users": users_copy,
+            },
+            default=lambda x: x.__dict__,
+            indent=None,
+        ),
+        status=HTTPStatus.CREATED,
+        mimetype="application/json",
+    )
     return response
