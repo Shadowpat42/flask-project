49c8b60e8c47ad2c4bcaa87001c01601ea06fdbc
codevoke <119005542+codevoke@users.noreply.github.com>
2024-03-08 17:40:54 +0000

    update configuration of application and .gitignore

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 from app import app
+from models import db
+
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+with app.app_context():
+    db.init_app(app)
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
